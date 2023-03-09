81df42f512dcce5ed7f4b9be0460c9b8445a1c70
Buliway <maks21062011@yandex.ru>
2023-03-09 04:23:32 +0300

    Game layer changes

--- vial/keymap.c
+++ vial/keymap.c
@@ -106,11 +106,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_GAME] = LAYOUT(
   //,-----------------------------------------------------------.                    ,-----------------------------------------------------------.
-      KC_TAB,    KC_T,     KC_Q,     KC_W,     KC_E,     KC_R,                         XXXXXXX,  XXXXXXX,   KC_UP,   XXXXXXX,  XXXXXXX, DF(L_ENG),
+      KC_TAB,    KC_T,     KC_Q,     KC_W,     KC_E,     KC_R,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   DF(L_ENG),
   //|---------+---------+---------+---------+---------+---------|                    |---------+---------+---------+---------+---------+---------|
-      KC_LSFT,   KC_G,     KC_A,     KC_S,     KC_D,     KC_F,                         XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_SCLN,  XXXXXXX,
+      KC_LSFT,   KC_G,     KC_A,     KC_S,     KC_D,     KC_F,                          KC_H,     KC_J,     KC_UP,    KC_K,     KC_L,    KC_GRV,
   //|---------+---------+---------+---------+---------+---------|                    |---------+---------+---------+---------+---------+---------|
-      KC_LCTL,   KC_B,     KC_Z,     KC_X,     KC_C,     KC_V,                         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+      KC_LCTL,   KC_B,     KC_Z,     KC_X,     KC_C,     KC_V,                          KC_N,    KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_M,    KC_SLSH,
   //|---------+---------+---------+---------+---------+---------+---------||---------+---------+---------+---------+---------+---------+---------|
                                               KC_LALT,  KC_SPC,    TD(4),      KC_H,   KC_SPC,   XXXXXXX
                                          //`+---------+---------+---------||---------+---------+---------+'
